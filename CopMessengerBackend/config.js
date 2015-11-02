065928c400fea03ebef7ba33c90ff88a194e6a59
synergiesTechnologies <synergiestechnology@gmail.com>
2015-11-02 20:20:04 +0400

    My Node initial commit with angular code

---
+++ CopMessengerBackend/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+
+	'secret': 'ilovescotchyscotch',
+	'database': 'mongodb://localhost:27017/contactlist'
+//	'database': 'mongodb://noder:noderauth@proximus.modulusmongo.net:27017/so9pojyN'
+
+};
